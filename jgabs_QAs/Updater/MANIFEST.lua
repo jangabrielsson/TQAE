14966638311b6e660ad978793a6eb9a65c5007b0
jangabrielsson <jan@gabrielsson.com>
2021-11-13 10:42:49 +0100

    fix

--- jgabs_QAs/Updater/MANIFEST.lua
+++ jgabs_QAs/Updater/MANIFEST.lua
@@ -16,7 +16,7 @@ return {
         interfaces= {"quickApp"},
         quickAppVariables = {},
       },
-      { version = 0.61,
+      { version = 0.62,
         vars= {
           base1 = "https://github.com/jangabrielsson/TQAE/raw/352f026cf2637a1149ce388733a8311b93821d3d"
         },
@@ -28,13 +28,13 @@ return {
         interfaces= {"quickApp"},
         quickAppVariables = {},
       },
-      { version = 0.62,
-        vars = {
-          base1 = "https://github.com/jangabrielsson/TQAE/raw/1117a60af5e05fa3196f2c2cfc5766126c4272c3"
-        },
-        descr = "Test version EventRunner4",
-        ref = 0.61
-      }
+--      { version = 0.63,
+--        vars = {
+--          base1 = "https://github.com/jangabrielsson/TQAE/raw/1117a60af5e05fa3196f2c2cfc5766126c4272c3"
+--        },
+--        descr = "Test version EventRunner4",
+--        ref = 0.61
+--      }
     }
   },
 
