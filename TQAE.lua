2526447b14970f71173b4b8d1f217824d29e2827
Jan Gabrielsson <jan@gabrielsson.com>
2022-02-19 13:49:20 +0100

    Fix text  color

--- TQAE.lua
+++ TQAE.lua
@@ -103,7 +103,7 @@ QuickApp options: (set with --%% directive in file)
 --]]
 
 local embedded=...              -- get parameters if emulator included from QA code...
-local version = "0.49"
+local version = "0.50"
 local EM = { cfg = embedded or {} }
 local cfg,pfvs = EM.cfg
 local function DEF(x,y) if x==nil then return y else return x end end
